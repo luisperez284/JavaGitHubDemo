51dbdb66abfe62f48be0e5c86bd272bb4b2b8cce
luisp <luis.fperez284@gmail.com>
2020-05-11 21:15:36 -0700

    Added bank account

---
+++ Exercise3/src/main/java/BankAccount.java
@@ -0,0 +1,11 @@
+public class BankAccount {
+    private double balance;
+    
+    /**
+     * Constructs a bank account with a zero balance
+     */
+    public BankAccount()
+    {
+        balance = 0;
+    }
+}
